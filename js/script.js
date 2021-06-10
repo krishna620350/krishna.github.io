e9042c64c69be70f8be1608141fc3ac24eef8f26
krishna620350 <manishkumar620350@gmail.com>
2021-06-10 15:22:29 +0530

    krishna

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var typed = new Typed("#name", {
+        strings: [
+          "My name Is Krishna",
+          "I am a software developer",
+          "Feel free to contact me"
+        ],
+        typeSpeed: 80,
+        backSpeed: 60,
+        loop:true,
+    });
+})
