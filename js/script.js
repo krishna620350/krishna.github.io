fedf421b13878c257252687d9ca5ded07d7029d5
krishna620350 <manishkumar620350@gmail.com>
2021-06-15 10:47:13 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -9,4 +9,16 @@ $(document).ready(function () {
         backSpeed: 60,
         loop:true,
     });
+    $(".read-cont").hide();
+    $("#read-end").hide();
+    $(".read-start").on("click",function() {
+      $(".read-cont").fadeToggle("slow");
+      $(".read-start").hide();
+      $("#read-end").show();
+    })
+    $("#read-end").on("click",function() {
+      $(".read-cont").fadeToggle("slow");
+      $(".read-start").show();
+      $("#read-end").hide();
+    });
 })
